8152bc6dd4349217dfea27bed24134c782275de3
tchoune <contact@yannickperret.com>
2021-11-24 10:09:07 +0100

    v0.2 add des cards services

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,28 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Data from '../Helpers/data.json'
 import Cards from './Cards';
 
 const Service = () => {
+    const [serviceIsOpen, setServiceIsopen] = useState(false);
+    const [serviceChoise , setServiceChoise] = useState([]);
 
 
+    const changeService = (serviceId) =>{
+        setServiceChoise(serviceId);
+        setServiceIsopen(true);
+     }
 
     return (
-        <div className="service-container">
-            <h2 className="service-container-title">Découvrez nos services</h2>
+        <div className="jp-services" >
+            <div className="container" >
+                <div className="body-title">
+                    <h2>Découvrez nos services</h2>
+                </div>
+                
+                {serviceIsOpen && 
+                <>
+                <div className="btn-container"><button onClick={(() => {setServiceIsopen(false)})} >{"< Revenir à la liste >"}</button></div>
+                <section className="service-show">
+                    <header className="show-header"> 
+                        <img src={"./img/services/"+serviceChoise.image} alt={"Image en cover de la prestation "+serviceChoise.title}/>
+                        <h3>{serviceChoise.title}</h3>
+                    </header>
+                    <article className="show-body">
+                        {serviceChoise.content}
+                    
+                        
+                        
+                    </article>
+                </section>
+                </>}
 
-            <section className="service-article">
-                    {Data.Services
-                    .sort((a, b) => b.id - a.id)
-                    .map((service) => (
-                        <Cards service={service} />
-                    ))}
-            </section>
 
-            <section className="service-show">
-                <article>
-                        ICI LES EXPLICATIONS DES CARDS
-                </article>
-            </section>
+                <section className="service-article">
+                        {Data.Services
+                        .sort((a, b) => a.id - b.id)
+                        .map((service) => (
+                            serviceIsOpen && serviceChoise.id === service.id ?
+                                null :
+                            <Cards 
+                            key={service.id}
+                            service={service}
+                            isOpen = {serviceIsOpen} 
+                            onSelectedService={changeService}/>
+                        ))}
+                </section>
+            </div>
+            
+            
         </div>
     );
 };
